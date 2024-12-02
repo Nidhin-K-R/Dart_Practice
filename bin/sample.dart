48a7c6226620f5b46fcbe90a80d749bf6f8370da
Nidhin K R <www.nidhinkr78@gmail.com>
2024-12-02 12:10:20 +0530

    List , for loop,merging two arrays

--- bin/sample.dart
+++ bin/sample.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 
 void main() {
-  int a, b;
-  print('enter a number');
-  a = int.parse(stdin.readLineSync()!);
-  print('enter second number');
-  b = int.parse(stdin.readLineSync()!);
-  print('Total sum is = ${a + b}');
+  List<int> array = [2, 4, 3, 6, 9];
+  //print(array);
+  array.add(10);
+  for (int i = 0; i < array.length; i++) {
+    print(array[i]);
+  }
+// value finding;
+  print(array.contains(10));
+  // merging to arrays
+  List<int> a = [100, 200, 300];
+
+  List<int> b = [...array, ...a];
+  for (int i = 0; i < b.length; i++) {
+    print(b[i]);
+  }
 }
